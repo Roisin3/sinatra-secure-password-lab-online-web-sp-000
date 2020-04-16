6ab1d4f99bb8fa59c5202248c1ac7b5556a6d721
Michael Lynch <michael@michaelrlynch.com>
2020-04-16 19:46:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,8 +38,8 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
-    user = User.find_by(:username => params[:username])
-    if user
+    @user = User.find_by(:username => params[:username])
+    if @user
       redirect '/account'
     else
       redirect 'failure'
